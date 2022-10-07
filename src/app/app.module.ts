d859202966a57d7b454c56c065ae95998d388df7
German Cruz <43277807+cruzgerman216@users.noreply.github.com>
2022-10-06 20:26:10 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TodoformComponent } from './todoform/todoform.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { TodoComponent } from './shared/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoformComponent,
+    TodolistComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule
